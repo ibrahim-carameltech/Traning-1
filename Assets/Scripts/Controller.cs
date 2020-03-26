93f2b75a3c4bf982a870196f55d854a76a7726a7
Muhammad Ibrahim <muhammad.ibrahim@carameltec.com>
2020-03-26 17:30:12 +0500

    Event Base Touch Controller

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -5,70 +5,37 @@ using UnityEngine.UI;
 
 public class Controller : MonoBehaviour
 {
-
+    [SerializeField]
     float speed = 50f;
-    float movePercentage = 20f;
 
-    Vector2 startPosition;
-    Vector2 endPosition;
-
-    void Update()
+    void Awake()
     {
-        if(Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
+        TouchController.onMove += move;
+    }
 
-            if(touch.phase == TouchPhase.Began)
-            {
-                startPosition = touch.position;
-            }
-            else if(touch.phase == TouchPhase.Ended)
-            {
-                endPosition = touch.position;
-                float xMoved = endPosition.x - startPosition.x;
-                float yMoved = endPosition.y - startPosition.y;
-                bool swipedHorizontal = Mathf.Abs(xMoved) > Mathf.Abs(yMoved);
-
-                float distance = Mathf.Sqrt((xMoved * xMoved) + (yMoved * yMoved));
-
-                bool percentageHorizontal = (distance > ((Screen.width / 100) * movePercentage));
-                bool percentageVertical = (distance > ((Screen.height / 100) * movePercentage));
-
-                if(swipedHorizontal && xMoved > 0 && percentageHorizontal)
-                {
-                    transform.Translate(Vector3.right * speed * Time.deltaTime);
-                }
-                else if(swipedHorizontal && xMoved < 0 && percentageHorizontal)
-                {
-                    transform.Translate(Vector3.left * speed * Time.deltaTime);
-                }
-                else if(!swipedHorizontal && yMoved > 0 && percentageVertical)
-                {
-                    transform.Translate(Vector3.forward * speed * Time.deltaTime);
-                }
-                else if(!swipedHorizontal && yMoved < 0 && percentageVertical)
-                {
-                    transform.Translate(Vector3.back * speed * Time.deltaTime);
-                }
-            }
-        }
-
-        if(Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            transform.Translate(Vector3.forward * speed * Time.deltaTime);
-        }
-        else if(Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            transform.Translate(Vector3.back * speed * Time.deltaTime);
-        }
-        else if(Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            transform.Translate(Vector3.right * speed * Time.deltaTime);
-        }
-        else if(Input.GetKeyDown(KeyCode.LeftArrow))
+    void move(TouchController.Direction x)
+    {
+        if(x == TouchController.Direction.Left)
         {
             transform.Translate(Vector3.left * speed * Time.deltaTime);
         }
+        else if(x == TouchController.Direction.Right)
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+        else if(x == TouchController.Direction.Forward)
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+        else if(x == TouchController.Direction.Backward)
+        {
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+        Debug.Log("Test " + x);
     }
 
+    void OnDestroy()
+	{
+        TouchController.onMove -= move;
+    }
 }
---
+++ Assets/Scripts/TouchController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchController : MonoBehaviour
+{
+    public enum Direction {Left, Right, Forward, Backward};
+
+    public delegate void OnMovement(Direction x);
+    public static event OnMovement onMove;
+
+
+    Vector2 startPosition;
+    Vector2 endPosition;
+
+    [SerializeField]
+    [Range(0.0f, 100.0f)]
+    float movePercentage = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if(touch.phase == TouchPhase.Began)
+            {
+                startPosition = touch.position;
+            }
+            else if(touch.phase == TouchPhase.Ended)
+            {
+                endPosition = touch.position;
+                float xMoved = endPosition.x - startPosition.x;
+                float yMoved = endPosition.y - startPosition.y;
+                bool swipedHorizontal = Mathf.Abs(xMoved) > Mathf.Abs(yMoved);
+
+                float distance = Mathf.Sqrt((xMoved * xMoved) + (yMoved * yMoved));
+
+                bool percentageHorizontal = (distance > ((Screen.width / 100) * movePercentage));
+                bool percentageVertical = (distance > ((Screen.height / 100) * movePercentage));
+
+                if(swipedHorizontal && xMoved > 0 && percentageHorizontal && onMove != null)
+                {
+                    onMove(Direction.Right);
+                }
+                else if(swipedHorizontal && xMoved < 0 && percentageHorizontal && onMove != null)
+                {
+                    onMove(Direction.Left);
+                }
+                else if(!swipedHorizontal && yMoved > 0 && percentageVertical && onMove != null)
+                {
+                    onMove(Direction.Forward);
+                }
+                else if(!swipedHorizontal && yMoved < 0 && percentageVertical && onMove != null)
+                {
+                    onMove(Direction.Backward);
+                }
+            }
+        }
+    }
+
+}
